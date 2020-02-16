5acabe4a780466c2f80fc1052ce8dbd29f944da8
v15hv4 <vishva2912@gmail.com>
2020-02-16 07:01:07 +0530

    Code cleanup and minor tweaks

--- alpha.py
+++ alpha.py
@@ -2,7 +2,7 @@ import pygame
 import random
 pygame.init()
 
-# Window Dimensions
+# Window dimensions
 win_height = 696
 win_width = 696
 win = pygame.display.set_mode((win_width, win_height))
@@ -11,12 +11,12 @@ pygame.display.set_caption("River Crossing [ALPHA]")
 
 clock = pygame.time.Clock()
 
-# Miscellaneous Global Variables
+# Misc global variables
 player_speed = 5
-# player_jump_speed = 4
 run = True
 current_player = 1
 
+# Blueprint of every in-game entity
 class entity(object):
     def __init__(self, x, y, dimens, sprite):
         self.x = x
@@ -26,7 +26,6 @@ class entity(object):
         self.speed = player_speed
         self.score = 0
         self.level = 1
-        # self.jump_speed = player_jump_speed
         self.up = False
         self.down = False
         self.left = False
@@ -50,7 +49,6 @@ class entity(object):
     def init_as_player(self, up, down):
         self.up = up
         self.down = down
-        # self.is_jumping = 0
 
     def init_as_enemy(self, speed, direction):
         self.speed = speed
@@ -70,16 +68,41 @@ class entity(object):
         elif self.right:
             win.blit(self.sprite[1], (self.x, self.y))
 
-        # Visualize hitbox_statices [DEBUG]
-        # pygame.draw.rect(win, (255, 0, 0), self.hitbox_static(), 2)
-
-# Sprite Dimensions
+# Sprite dimensions
 player_dimens = (28, 32)
 orca_dimens = (95, 56)
 turtle_dimens = (70, 40)
 whale_dimens = (86, 70)
 crab_dimens = (54, 30)
 boat_dimens = (80, 90)
+
+# Initial player positions
+player1_x = (win_width / 2)
+player1_y = 655
+player2_x = (win_width / 2)
+player2_y = 8
+
+# Water Rows
+water_r1 = 40
+water_r2 = 148
+water_r3 = 256
+water_r4 = 364
+water_r5 = 472
+water_r6 = 580
+
+# Land Rows
+land_r1 = 118
+land_r2 = 226
+land_r3 = 334
+land_r4 = 442
+land_r5 = 550
+
+# Enemies' Row Offsets
+orca_offset = 0
+turtle_offset = 20
+whale_offset = -10
+crab_offset = 0
+boat_offset = -35
 
 # Background
 bg = pygame.image.load('res/main_bg.png').convert()
@@ -141,73 +164,19 @@ boat_right =  pygame.image.load('res/boat_right.png')
 boat_right = pygame.transform.scale(boat_right, boat_dimens)
 boat_sprite = [boat_left, boat_right]
 
-# Initial Positions
-p1_x = (win_width / 2)
-p1_y = 655
-p2_x = (win_width / 2)
-p2_y = 8
-
-# Water Rows
-water_r1 = 40
-water_r2 = 148
-water_r3 = 256
-water_r4 = 364
-water_r5 = 472
-water_r6 = 580
-
-# Land Rows
-land_r1 = 118
-land_r2 = 226
-land_r3 = 334
-land_r4 = 442
-land_r5 = 550
-
-# Enemies' Row Offsets
-orca_offset = 0
-turtle_offset = 20
-whale_offset = -10
-crab_offset = 0
-boat_offset = -35
-
 # Entity lists
-orca = [
-    orca_dimens, 
-    orca_offset, 
-    orca_sprite
-]
-turtle = [
-    turtle_dimens, 
-    turtle_offset, 
-    turtle_sprite
-]
-whale = [
-    whale_dimens, 
-    whale_offset, 
-    whale_sprite
-]
-boat = [
-    boat_dimens, 
-    boat_offset, 
-    boat_sprite
-]
-crab = [
-    crab_dimens, 
-    crab_offset, 
-    crab_sprite
-]
-entity_list = [
-    orca, 
-    turtle, 
-    whale, 
-    boat, 
-    crab
-]
-entity_speeds = [3, 5, 7, 9, 11]
+orca = [orca_dimens, orca_offset, orca_sprite]
+turtle = [turtle_dimens, turtle_offset, turtle_sprite]
+whale = [whale_dimens, whale_offset, whale_sprite]
+boat = [boat_dimens, boat_offset, boat_sprite]
+crab = [crab_dimens, crab_offset, crab_sprite]
+entity_list = [orca, turtle, whale, boat, crab]
+entity_speeds = [3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29]
 
 # Players
-player1 = entity(p1_x, p1_y, player_dimens, blue_sprite)
+player1 = entity(player1_x, player1_y, player_dimens, blue_sprite)
 player1.init_as_player(True, False)
-player2 = entity(p2_x, p2_y, player_dimens, purple_sprite)
+player2 = entity(player2_x, player2_y, player_dimens, purple_sprite)
 player2.init_as_player(False, True)
 
 # Enemies
@@ -350,10 +319,12 @@ def sprite_direction(new_direction):
     elif new_direction == 'down':
         player.down = True
 
+# Update speeds of moving enemies
 def update_speeds():
     for entity in moving_entity_list:
         entity.speed = entity_speeds[player.level - 1]
 
+# Calculate score based on player's position
 def update_score():
     if player == player1:
         if player.y < 580 and player.y > 550:
@@ -381,7 +352,7 @@ def update_score():
             player.is_successful = True
             player.is_dead = True
     else:
-        if player.y > 580:
+        if player.y > 630:
             player.score = 80
             player.is_successful = True
             player.is_dead = True
@@ -408,20 +379,22 @@ def update_score():
         elif player.y < 40:
             player.score = 0
 
+# Reinitialize player values for the next round
 def next_round():
     player1.is_dead = False
     player1.is_successful = False
     player1.has_played = False
     player1.score = 0
-    player1.x = p1_x
-    player1.y = p1_y
+    player1.x = player1_x
+    player1.y = player1_y
     player2.is_dead = False
     player2.is_successful = False
     player2.has_played = False
     player2.score = 0
-    player2.x = p2_x
-    player2.y = p2_y
+    player2.x = player2_x
+    player2.y = player2_y
 
+# Switch player when one's turn is done
 def switch_player():
     global current_player
     if player1.is_dead and player2.is_dead:
@@ -435,43 +408,10 @@ def switch_player():
 
 # Restrict player from accessing certain regions of the map
 def restrict_player():
-    #if not player.is_jumping:
     if player.y < 0:
         player.y = 0
     if player.y > (win_height - player_dimens[1] - 2):
         player.y = (win_height - player_dimens[1] - 2)
-        '''
-        if player.y > 558 and player.y < 640:
-            if player.up:
-                player.y = 642
-            elif player.down:
-                player.y = 556
-        if player.y > 450 and player.y < 532:
-            if player.up:
-                player.y = 534
-            elif player.down:
-                player.y = 448
-        if player.y > 342 and player.y < 424:
-            if player.up:
-                player.y = 426
-            elif player.down:
-                player.y = 340
-        if player.y > 234 and player.y < 316:
-            if player.up:
-                player.y = 318
-            elif player.down:
-                player.y = 232
-        if player.y > 126 and player.y < 208:
-            if player.up:
-                player.y = 210
-            elif player.down:
-                player.y = 124
-        if player.y > 18 and player.y < 100:
-            if player.up:
-                player.y = 102
-            elif player.down:
-                player.y = 16
-        '''
 
 # Redraw surface
 def redraw(result_text):
@@ -487,17 +427,12 @@ def redraw(result_text):
         crab3.draw()
         crab4.draw()
         crab5.draw()
-        '''
-        if not player.is_jumping:
-            player.draw()
-        '''
         row1_enemy.draw()
         row2_enemy.draw()
         row3_enemy.draw()
         row4_enemy.draw()
         row5_enemy.draw()
         row6_enemy.draw()
-        #if player.is_jumping:
         player.draw()
     else:
         win.fill((0, 0, 0))
@@ -513,12 +448,13 @@ def redraw(result_text):
         win.blit(continue_text, (((win_width - continue_text.get_width() / 2) / 2) - 40, (win_height / 2) + 100))
     pygame.display.update()
 
-# Main Loop
-font = pygame.font.Font('HeartbitXX.ttf', 30)
+# Main loop
+font = pygame.font.Font('HeartbitXX.ttf', 32)
 
 while(run):
     time_bonus = clock.tick(60)
 
+    # Check for player change
     if current_player == 1:
         player = player1
         player1.has_played = True
@@ -526,59 +462,22 @@ while(run):
         player = player2
         player2.has_played = True
 
+    # Update enemy speeds according to player's level
     update_speeds()
     
     # Single keypress actions
     for event in pygame.event.get():
 
         # Quit Game
-        if event.type == pygame.QUIT:
+        if (event.type == pygame.QUIT) or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             run = False
 
-        '''
-        # Jump Trigger
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if not player.right and not player.left:
-                    if player.up and player.y > 50:
-                        player.is_jumping = 1
-                    elif player.down and player.y < (win_height - 50):
-                        player.is_jumping = 2
-        '''
-
-        # Switch Player [DEBUG]
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_0:
-                if current_player == 1:
-                    current_player = 2
-                else:
-                    current_player = 1
-
     if not player.is_dead:
-
-        '''
-        # Jump
-        delta = clock.tick(60)
-        if player.is_jumping == 1:
-            if player.jump_speed > -2:
-                player.y -= player.jump_speed * delta
-                player.jump_speed -= 2
-            else:
-                player.is_jumping = False
-                player.jump_speed = player_jump_speed
-        elif player.is_jumping == 2:
-            if player.jump_speed > -2:
-                player.y += player.jump_speed * delta
-                player.jump_speed -= 2
-            else:
-                player.is_jumping = False
-                player.jump_speed = player_jump_speed
-        '''
 
         # Restrict motion of player
         restrict_player()
 
-        # Enemy motion
+        # Enemy movement
         for enemy_i in moving_entity_list:
             if enemy_i.x < win_width and enemy_i.direction == 1:
                 enemy_i.x += enemy_i.speed
@@ -595,9 +494,7 @@ while(run):
         for enemy_i in moving_entity_list:
             if player.com()[0] > enemy_i.hitbox_static()[0] and player.com()[0] < enemy_i.hitbox_static()[0] + enemy_i.hitbox_static()[2]:
                 if player.com()[1] > enemy_i.hitbox_static()[1] and player.com()[1] < enemy_i.hitbox_static()[1] + enemy_i.hitbox_static()[3]:
-                    # if player.is_jumping:
                     player.is_dead = True
-
         for enemy_i in static_entity_list:
             if player.com()[0] > enemy_i.hitbox_static()[0] and player.com()[0] < enemy_i.hitbox_static()[0] + enemy_i.hitbox_static()[2]:
                 if player.com()[1] > enemy_i.hitbox_static()[1] and player.com()[1] < enemy_i.hitbox_static()[1] + enemy_i.hitbox_static()[3]:
@@ -623,11 +520,14 @@ while(run):
                 player.y += player.speed
         result_text = font.render('', 1, (255, 255, 255))
 
+        # Update score based on player's position
         update_score()
 
+        # Add time bonus to total score if successfully reached
         if player.is_successful:
             player.score += (100 - time_bonus)
 
+        # Decide winner of the round
         if player1.is_dead and player2.is_dead:
             if (player1.is_successful and not player2.is_successful) or (player1.score > player2.score):
                 result_text = font.render('PLAYER 1 WINS THE ROUND!', 1, (255, 255, 255))
@@ -647,3 +547,11 @@ while(run):
 
     redraw(result_text)
 pygame.quit()
+
+# TODO: Replace boat sprite
+# TODO: Map custom keys to u/d/l/r navigation for each player
+# TODO: Add config files
+# TODO: Check whether code complies with PEP8 standards
+# TODO: Sounds (?)
+# TODO: Splash screen (?)
+# TODO: Tutorial screen on initial run (?)
